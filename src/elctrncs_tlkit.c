6d08edc398c6e09ac800732e03671e5c0f642511
Ian Mitchell <Ian.Mitchell_001@gmx.com>
2018-04-18 21:28:06 -0400

    Add the `src` directory.

---
+++ src/elctrncs_tlkit.c
@@ -0,0 +1,72 @@
+# include <stdio.h>
+# include <math.h>
+/* This program is more or less a calculator for electronics. It will do most basic calculations in DC and AC along with some electromagnetism. I will be adding more functions as I go along with learning electronics since I'm still a student in the EE shop at my high school. 
+
+I cannot guaruntee that this will be as good as something like SPICE. It is more for my electronics class and for furutre projects.*/
+/*
+
+Electronics toolkit. A simple electronics calculator. 
+Copyright (C) 2018 Ian Mitchell <Ian.Mitchell_001@gmx.com>
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+// The `float` is used to essentially just give it support for floating point numbers. I don't have much success with GCC otherwise. 
+float main(){
+
+	return 0;
+}
+
+// Functions below
+// ---------------
+
+// The menu function for selecting a thing to calculate. 
+float menu();
+float menu(){
+
+}
+
+// Ohm's law. No electronics program is complete without it!
+
+float ohm();
+float ohm(){
+
+}
+
+// DC circuit calculations. Will work with series and parallel circuits. 
+
+float dc();
+float dc(){
+
+}
+
+// AC circuit calculations
+
+float ac();
+float ac(){
+
+}
+
+// Capacitor and inductor calculations. **TODO:** RL, RC, and RLC circuits
+
+float capnind();
+float capnind(){
+
+}
+
+// Basic electromagnetism calculations
+
+float electromag();
+float electromag(){
+
+}
